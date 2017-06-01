f9549789f0251022a40692edbff0e75d4b3a9a0a
Hendrik Kunert <hendrik.kunert@henku.de>
2017-06-01 20:12:47 +0200

    Fix private field in Tile struct

--- src/style/tile.rs
+++ src/style/tile.rs
@@ -3,7 +3,7 @@ const PAGE_SIZE: usize = 256;
 const IMAGE_SIZE: usize = 64;
 
 #[derive(Debug)]
-pub struct Tile(Vec<u8>);
+pub struct Tile(pub Vec<u8>);
 
 impl Tile {
     pub fn load_from_page(id: usize, page: &[u8]) -> Tile {
