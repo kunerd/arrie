9ca4e4c722b97bc2b508747859b115aa3c094dfd
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-01-10 09:29:47 +0100

    Add color lookup from palletes

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,28 @@ const DATA_PATH: &str = "data/bil.sty";
 
 fn main() {
     let file = File::open(DATA_PATH).unwrap();
-
-    let style = StyleFile::from_file(&file);
+    let mut style = StyleFile::from_file(&file);
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 512, 512)
+        .window("rust-sdl2 demo: Video", 1024, 1024)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().software().build().unwrap();
+    //canvas.set_draw_color(Color::WHITE);
+    //canvas.fill_rect(None).unwrap();
     let texture_creator = canvas.texture_creator();
-    canvas.clear();
 
-    //let mut windows = Vec::new();
+    let info = canvas.info();
+    dbg!(info.texture_formats);
 
-    //for Tile(ref tile) in style.tiles.iter().take(16) {
-    //    windows.push(show_tile(&video_subsystem, tile));
-    //}
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
     let mut tile_iter = style.tiles.iter().enumerate();
+
     'mainloop: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -50,33 +48,33 @@ fn main() {
                     ..
                 } => match tile_iter.next() {
                     Some((index, tile)) => {
-                        //let _ = texture.update(None, &tile.0, IMAGE_SIZE);
-                        let palette_base = style.palette_base.tile;
-                        let virtual_palette_index = index + palette_base as usize;
-                        let palette_index = style
-                            .palette_index
-                            .physical_index
-                            .get(index)
-                            .unwrap();
+                        let palette_index = style.palette_index.physical_index.get(index).unwrap();
+
                         let phys_palette =
-                            style.physical_palette.get(*palette_index as usize).unwrap();
-                        let palette: Vec<_> = phys_palette.colors.iter().map(map_color).collect();
-                        let palette = Palette::with_colors(&palette).unwrap();
-                        //surface.set_palette(&palette).unwrap();
-                        let mut surface = Surface::new(
+                            style.physical_palette.get_mut(*palette_index as usize).unwrap();
+
+                        let mut colored_image: Vec<u8> = tile
+                            .0
+                            .iter()
+                            .map(|p| *phys_palette.colors.get(*p as usize).unwrap())
+                            .flat_map(|c| c.to_ne_bytes())
+                            .collect();
+
+                        let mut surface = Surface::from_data(
+                            colored_image.as_mut_slice(),
                             IMAGE_SIZE as u32,
                             IMAGE_SIZE as u32,
-                            PixelFormatEnum::RGB24,
+                            IMAGE_SIZE as u32 * 4,
+                            PixelFormatEnum::BGRA8888,
                         )
                         .unwrap();
-                        surface.set_palette(&palette).unwrap();
+                        surface.set_color_key(true, Color::RGBA(0, 0, 0, 0)).unwrap();
 
-                        let mut texture = texture_creator
-                            .create_texture_from_surface(surface)
-                            .unwrap();
-                        texture.update(None, &tile.0, IMAGE_SIZE).unwrap();
-
+                        let texture = surface.as_texture(&texture_creator).unwrap();
+                        dbg!(texture.query());
+                        canvas.clear();
                         canvas.copy(&texture, None, None).expect("Render failed");
+                        canvas.present();
                     }
                     None => break 'mainloop,
                 },
@@ -89,15 +87,15 @@ fn main() {
             }
         }
 
-        canvas.present();
+        //canvas.present();
         std::thread::sleep(Duration::from_millis(100));
     }
 }
 
 fn map_color(v: &u32) -> Color {
-    let r = (v & 0xff000000) >> 24;
-    let g = (v & 0x00ff0000) >> 16;
-    let b = (v & 0x0000ff00) >> 8;
+    let b = (v & 0xff000000) >> 24;
+    let g = (v & 0x0000ff00) >> 8;
+    let r = (v & 0x00ff0000) >> 16;
     let a = (v & 0x000000ff) >> 0;
 
     Color::RGBA(r as u8, g as u8, b as u8, a as u8)
--- src/style/tile.rs
+++ src/style/tile.rs
@@ -6,4 +6,23 @@ const IMAGE_SIZE: usize = 64;
 pub struct Tile(pub Vec<u8>);
 
 impl Tile {
+<<<<<<< HEAD
+=======
+    pub fn from_file(id: usize, page :&[u8]) -> Self {
+        let y_start = (id / 4) * IMAGE_SIZE;
+        let y_end = y_start + IMAGE_SIZE;
+        let mut tile = Vec::with_capacity(IMAGE_SIZE * IMAGE_SIZE);
+
+        for y in y_start..y_end {
+            let x_start = (id % 4) * IMAGE_SIZE;
+            let x_end = x_start + IMAGE_SIZE;
+
+            for x in x_start..x_end {
+                tile.push(page[(y * PAGE_SIZE) + x]);
+            }
+        }
+
+        Tile(tile)
+    }
+>>>>>>> 2346cc8 (Add color lookup from palletes)
 }
