24e86ac8eb469651342ab77b98e014ef3bac3ee6
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-02-23 16:15:41 +0100

    Add basic drawing shader

---
+++ assets/shaders/extended_material.wgsl
@@ -0,0 +1,67 @@
+#import bevy_pbr::{
+    pbr_fragment::pbr_input_from_standard_material,
+    pbr_functions::alpha_discard,
+}
+
+#ifdef PREPASS_PIPELINE
+#import bevy_pbr::{
+    prepass_io::{VertexOutput, FragmentOutput},
+    pbr_deferred_functions::deferred_output,
+}
+#else
+#import bevy_pbr::{
+    forward_io::{VertexOutput, FragmentOutput},
+    pbr_functions::{apply_pbr_lighting, main_pass_post_lighting_processing},
+}
+#endif
+
+struct MaterialSettings {
+    flip: u32,
+    angle: f32,
+}
+
+@group(2) @binding(100)
+var<uniform> material: MaterialSettings ;
+
+const TAU:f32 =  6.28318530718;
+
+/// Clockwise by `theta`
+fn rotate2D(theta: f32) -> mat2x2<f32> {
+    let c = cos(theta);
+    let s = sin(theta);
+    return mat2x2<f32>(c, s, -s, c);
+}
+
+@fragment
+fn fragment(
+    in: VertexOutput,
+    @builtin(front_facing) is_front: bool,
+) -> FragmentOutput {
+    var uvs = in.uv;
+
+    if material.angle >= 0.0 {
+    	uvs -= 0.5;
+    	uvs *= rotate2D(-material.angle);
+    	uvs += 0.5;
+    }
+
+    if material.flip == 1 {
+	uvs.y = 1.0 - uvs.y;
+    }
+
+    var modified_input = in;
+    modified_input.uv = uvs;
+
+    var pbr_input = pbr_input_from_standard_material(modified_input, is_front);
+
+#ifdef PREPASS_PIPELINE
+    // in deferred mode we can't modify anything after that, as lighting is run in a separate fullscreen shader.
+    let out = deferred_output(in, pbr_input);
+#else
+    var out: FragmentOutput;
+    // apply lighting
+    out.color = apply_pbr_lighting(pbr_input);
+#endif
+
+    return out;
+}
--- src/bin/3-sided-diagonal.rs
+++ src/bin/3-sided-diagonal.rs
@@ -1,11 +1,18 @@
 use std::thread::spawn;
 
-use bevy::prelude::*;
+use bevy::{
+    pbr::{ExtendedMaterial, MaterialExtension, OpaqueRendererMethod},
+    prelude::*,
+    render::render_resource::{AsBindGroup, ShaderRef, ShaderType},
+};
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(MaterialPlugin::<
+            ExtendedMaterial<StandardMaterial, MyExtension>,
+        >::default())
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, (setup_camera, load_scene))
         .run();
@@ -30,30 +37,53 @@ fn setup_camera(mut commands: Commands) {
 fn load_scene(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut ext_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, MyExtension>>>,
 ) {
-    let mesh: Handle<Mesh> = asset_server.load("3-side-diagonal.glb#Mesh0");
-    let material = materials.add(StandardMaterial {
-        base_color: Color::srgb(1.0, 0.0, 0.0),
+    let mesh: Handle<Mesh> = asset_server.load("gta2_block_model.glb#Mesh1/Primitive0");
+    let texture = asset_server.load("uv_check.png");
+    let material = StandardMaterial {
+        //base_color: Color::srgb(1.0, 0.0, 0.0),
+        base_color_texture: Some(texture),
+        opaque_render_method: OpaqueRendererMethod::Auto,
         ..StandardMaterial::default()
+    };
+    let ext_material = ext_materials.add(ExtendedMaterial {
+        base: material,
+        extension: MyExtension {
+            holder: MyExtensionHolder {
+                flip: 1,
+                angle: 0.25,
+            },
+        },
     });
-
-    //commands.spawn((
-    //    Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-    //    MeshMaterial3d(material.clone()),
-    //    Transform::from_xyz(1.0, 1.0, 1.0),
-    //));
-    //let my_gltf = asset_server.load("3-side-diagonal.glb#Scene0");
-    //commands.spawn(SceneBundle {
-    //    scene: SceneRoot(my_gltf),
-    //    transform: Transform::from_xyz(2.0, 0.0, -5.0),
-    //    ..Default::default()
-    //});
 
     commands.spawn((
         Mesh3d(mesh),
-        MeshMaterial3d(material),
+        MeshMaterial3d(ext_material),
         Transform::from_xyz(-1.0, -1.0, -1.0),
     ));
+}
+
+#[derive(Asset, AsBindGroup, Reflect, Debug, Clone)]
+struct MyExtension {
+    #[uniform(100)]
+    holder: MyExtensionHolder,
+}
+
+#[derive(ShaderType, Reflect, Default, Clone, Debug)]
+struct MyExtensionHolder {
+    flip: u32,
+    angle: f32,
+}
+
+const SHADER_ASSET_PATH: &str = "shaders/extended_material.wgsl";
+
+impl MaterialExtension for MyExtension {
+    fn fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
+    }
+
+    fn deferred_fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
+    }
 }
